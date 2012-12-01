cf6ab98014823e8e99ccb320f176b7c42de3d4bf
Brendan Forster <me@brendanforster.com>
2012-12-02 09:32:05 +1000

    switching to an even simpler implementation thanks to @machadogj pointing out that OfType<T> is available

--- src/Reactive.EventAggregator/EventAggregator.cs
+++ src/Reactive.EventAggregator/EventAggregator.cs
@@ -5,34 +5,19 @@ using System.Reactive.Subjects;
 namespace Reactive.EventAggregator
 {
     // based on http://joseoncode.com/2010/04/29/event-aggregator-with-reactive-extensions/
+    // and http://machadogj.com/2011/3/yet-another-event-aggregator-using-rx.html
     public class EventAggregator : IEventAggregator
     {
-        readonly ThreadSafeDictionary<Type, object> subjects = new ThreadSafeDictionary<Type, object>();
+        readonly ISubject<object> subject = new Subject<object>();
 
         public IObservable<TEvent> GetEvent<TEvent>()
         {
-            ISubject<TEvent> subject;
-
-            if (subjects.ContainsKey(typeof (TEvent)))
-            {
-                subject = (ISubject<TEvent>)subjects[typeof (TEvent)];
-            }
-            else
-            {
-                subject = new Subject<TEvent>();
-                subjects.Add(typeof(TEvent), subject);
-            }
-            
-            return subject.AsObservable();
+            return subject.OfType<TEvent>().AsObservable();
         }
 
         public void Publish<TEvent>(TEvent sampleEvent)
         {
-            object subject;
-            if (subjects.TryGetValue(typeof (TEvent), out subject))
-            {
-                ((ISubject<TEvent>)subject).OnNext(sampleEvent);
-            }
+            subject.OnNext(sampleEvent);
         }
     }
 }
