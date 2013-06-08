6cfbe405cacfed749cbb5d1023c75e80e8d59494
Nick Hodge <hodgenick@gmail.com>
2013-06-08 16:35:05 +1000

    .NET 4.5 because bored in airport

---
+++ src/EventAggregator.NET45/EventAggregator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+namespace Reactive.EventAggregator
+{
+    // based on http://joseoncode.com/2010/04/29/event-aggregator-with-reactive-extensions/
+    // and http://machadogj.com/2011/3/yet-another-event-aggregator-using-rx.html
+    public class EventAggregator : IEventAggregator
+    {
+        readonly ISubject<object> subject = new Subject<object>();
+
+        public IObservable<TEvent> GetEvent<TEvent>()
+        {
+            return subject.OfType<TEvent>().AsObservable();
+        }
+
+        public void Publish<TEvent>(TEvent sampleEvent)
+        {
+            subject.OnNext(sampleEvent);
+        }
+    }
+}
